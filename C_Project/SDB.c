ad5aa7682bf6753218eec19f53430a7f5d0c84d1
Behiry1 <omarbehiry53@gmail.com>
2023-09-07 03:43:20 +0300

    Final Project

--- C_Project/SDB.c
+++ C_Project/SDB.c
@@ -1,5 +1,7 @@
 #include "STD.h"
 #include "SDB.h"
+static uint32 entry = 0;
+
 void intialize_Struct()
 {
     for(int i = 0 ; i < 10 ; i++)
@@ -10,10 +12,10 @@ void intialize_Struct()
 
 bool SDB_IsFull()
 {
-    if(students[9].Student_ID == 0)
-        return false;
-    else
+    if(entry > 9)
         return true;
+    else
+        return false;
 
 }
 
@@ -21,7 +23,7 @@ uint8 SDB_GetUsedSize()
 {
     uint8 size = 0;
 
-    for(int i = 0 ;i< 9 ;i++)
+    for(int i = 0 ;i< 10 ;i++)
     {
         if(students[i].Student_ID != 0)
             size++;
@@ -35,76 +37,75 @@ uint8 SDB_GetUsedSize()
 
 bool SDB_AddEntry()
 {
-    static uint32 entry = 0;
 
     if(SDB_IsFull() || entry >9)
         return false;
     else
     {
-       printf("Please enter the student %d id\n",entry+1);
+       printf("Please enter the student %d id : ",entry+1);
        while(!scanf("%d",&students[entry].Student_ID) )
        {
            printf("Please Enter a valid input\n");
-           printf("Please enter the student %d id\n",entry+1);
+           printf("Please enter the student %d id : ",entry+1);
            flush();
        }
 
-       printf("Please enter the student %d year\n",entry+1);
+       printf("Please enter the student %d year : ",entry+1);
        while(!scanf("%d",&students[entry].Student_year) )
        {
            printf("Please Enter a valid input\n");
-           printf("Please enter the student %d year\n",entry+1);
+           printf("Please enter the student %d year : ",entry+1);
            flush();
        }
 
-       printf("Please enter the student %d course 1 id\n",entry+1);
+       printf("Please enter the student %d course 1 id : ",entry+1);
        while(!scanf("%d",&students[entry].Course1_ID) )
        {
            printf("Please Enter a valid input\n");
-           printf("Please enter the student %d course 1 id\n",entry+1);
+           printf("Please enter the student %d course 1 id : ",entry+1);
            flush();
        }
 
-       printf("Please enter the student %d course 1 grade\n",entry+1);
+       printf("Please enter the student %d course 1 grade : ",entry+1);
        while(!scanf("%d",&students[entry].Course1_grade) )
        {
            printf("Please Enter a valid input\n");
-           printf("Please enter the student %d course 1 grade\n",entry+1);
+           printf("Please enter the student %d course 1 grade : ",entry+1);
            flush();
        }
 
-       printf("Please enter the student %d course 2 id\n",entry+1);
+       printf("Please enter the student %d course 2 id : ",entry+1);
        while(!scanf("%d",&students[entry].Course2_ID) )
        {
            printf("Please Enter a valid input\n");
-           printf("Please enter the student %d course 2 id\n",entry+1);
+           printf("Please enter the student %d course 2 id : ",entry+1);
            flush();
        }
 
-       printf("Please enter the student %d course 2 grade\n",entry+1);
+       printf("Please enter the student %d course 2 grade : ",entry+1);
        while(!scanf("%d",&students[entry].Course2_grade) )
        {
            printf("Please Enter a valid input\n");
-           printf("Please enter the student %d course 2 grade\n",entry+1);
+           printf("Please enter the student %d course 2 grade : ",entry+1);
            flush();
        }
 
-       printf("Please enter the student %d course 3 id\n",entry+1);
+       printf("Please enter the student %d course 3 id : ",entry+1);
        while(!scanf("%d",&students[entry].Course3_ID) )
        {
            printf("Please Enter a valid input\n");
-           printf("Please enter the student %d course 3 id\n",entry+1);
+           printf("Please enter the student %d course 3 id : ",entry+1);
            flush();
        }
 
-       printf("Please enter the student %d course 3 grade\n",entry+1);
+       printf("Please enter the student %d course 3 grade : ",entry+1);
        while(!scanf("%d",&students[entry].Course3_grade) )
        {
            printf("Please Enter a valid input\n");
-           printf("Please enter the student %d course 3 grade\n",entry+1);
+           printf("Please enter the student %d course 3 grade : ",entry+1);
            flush();
        }
-
+       printf("\n");
     }
 
     entry++;
@@ -113,6 +114,7 @@ bool SDB_AddEntry()
 
 void SDB_DeletEntry (uint32 id)
 {
+
     int i = Get_Entry(id);
 
     students[i].Student_ID=0;
@@ -123,6 +125,8 @@ void SDB_DeletEntry (uint32 id)
     students[i].Course2_grade=0;
     students[i].Course3_ID=0;
     students[i].Course3_grade=0;
+    Shift_Data(i);
+    entry--;
 
 }
 
@@ -131,7 +135,7 @@ bool SDB_ReadEntry (uint32 id)
     if(SDB_IsIdExist(id))
     {
         int i = Get_Entry(id);
-        printf("%d\n%d\n%d\n%d\n%d\n&d\n%d\n%d\n",students[i].Student_ID,students[i].Student_year,students[i].Course1_ID,students[i].Course1_grade,students[i].Course2_ID,students[i].Course2_grade,students[i].Course3_ID,students[i].Course3_grade);
+        printf("Student's id : %d\nStudent's year : %d\nCourse 1 id : %d\nCourse 1 grade : %d\nCourse 2 id : %d\nCourse 2 grade : %d\nCourse 3 id : %d\nCourse 3 grade : %d\n\n",students[i].Student_ID,students[i].Student_year,students[i].Course1_ID,students[i].Course1_grade,students[i].Course2_ID,students[i].Course2_grade,students[i].Course3_ID,students[i].Course3_grade);
     }
     else
     {
@@ -144,10 +148,10 @@ bool SDB_ReadEntry (uint32 id)
 void SDB_GetList (uint8* Count, uint32* List)
 {
     uint8 ctr = 0;
-    int j = 0;
-    for(int i = 0;i<9;i++)
+    uint32 j = 0;
+    for(int i = 0;i<10;i++)
     {
-        if(students[i].Student_ID !=0)
+        if(students[i].Student_ID != 0)
         {
             List[j] = students[i].Student_ID;
             j++;
@@ -161,7 +165,7 @@ void SDB_GetList (uint8* Count, uint32* List)
 
 bool SDB_IsIdExist (uint32 id)
 {
-    for(int i = 0 ; i< 9;i++)
+    for(int i = 0 ; i< 10;i++)
     {
         if(students[i].Student_ID == id)
             return true;
@@ -179,10 +183,33 @@ void flush()
 
 int Get_Entry(uint32 id)
 {
-    for(int i = 0 ; i<9;i++)
+    for(int i = 0 ; i<10;i++)
     {
         if(students[i].Student_ID == id)
             return i;
     }
+}
+
+void Shift_Data(int start)
+{
+    for(int i = start ; i < 10 ; i++)
+    {
+        if(i == 9)
+        {
+            students[i].Student_ID = 0;
+            students[i].Course1_grade = 0;
+            students[i].Course1_ID = 0;
+            students[i].Course2_grade = 0;
+            students[i].Course2_ID = 0;
+            students[i].Course3_grade = 0;
+            students[i].Course3_ID = 0;
+            students[i].Student_year = 0;
+            break;
+        }
+
+        students[i] = students[i+1];
+
+    }
+
 }
 
--- C_Project/SDBAPP.c
+++ C_Project/SDBAPP.c
@@ -1,10 +1,14 @@
 #include "SDB.h"
+
 void SDB_APP ()
 {
+    printf("Welcome to the student database using c\n\n");
+    printf("Please enter the first 3 students data\n\n");
+
     intialize_Struct();
     SDB_AddEntry();
-    //SDB_AddEntry();
-    //SDB_AddEntry();
+    SDB_AddEntry();
+    SDB_AddEntry();
     int i;
 
     printf(" 1. To add entry, enter 1 \n 2. To get used size in database, enter 2 \n 3. To read student data, enter 3 \n 4. To get the list of all student IDs, enter 4 \n 5. To check is ID is existed, enter 5 \n 6. To delete student data, enter 6 \n 7. To check is database is full, enter 7 \n 8. To exit enter 0\n");
@@ -33,13 +37,68 @@ void SDB_APP ()
         printf(" 1. To add entry, enter 1 \n 2. To get used size in database, enter 2 \n 3. To read student data, enter 3 \n 4. To get the list of all student IDs, enter 4 \n 5. To check is ID is existed, enter 5 \n 6. To delete student data, enter 6 \n 7. To check is database is full, enter 7 \n 8. To exit enter 0\n");
 
     }
-    //SDB_DeletEntry()
+
 
 }
 
 void SDB_action (uint8 choice)
 {
+    uint32 id,list[10];
+    uint8 size;
+    bool IsFull,IsExsit;
 
+    switch(choice)
+    {
+    case 1:
+        IsFull = SDB_AddEntry();
+        if(IsFull == false)
+            printf("\nSorry the database is full\n\n");
+    break;
+    case 2:
+        printf("\nThe use of data is  : %d\n\n",(uint32)SDB_GetUsedSize());
+    break;
+    case 3:
+        printf("\nPlease enter the student's id : ");
+        scanf("%d",&id);
+        printf("\n");
+        IsExsit = SDB_ReadEntry(id);
+        if(IsExsit == false)
+            printf("\nSorry this id doesn't exsit\n");
+        printf("\n");
+    break;
+    case 4:
+        SDB_GetList(&size,&list);
+        printf("\nThe students ids in the database are : \n");
+        for(int i = 0 ; i < size ; i++)
+        {
+            printf("%d\n",list[i]);
+        }
+        printf("\n");
+    break;
+    case 5:
+        printf("\nPlease enter the id : ");
+        scanf("%d",&id);
+        printf("\n");
+        IsExsit = SDB_IsIdExist(id);
+        if(IsExsit == true)
+            printf("\nThe id exist in the database.\n\n");
+        else
+            printf("\nThe id doesn't exist in the database.\n\n");
+    break;
+    case 6:
+        printf("\nPlease enter the id wanted to be delete : ");
+        scanf("%d",&id);
+        printf("\n");
+        SDB_DeletEntry(id);
 
+    break;
+    case 7:
+        IsFull = SDB_IsFull();
+        if(IsFull == true)
+            printf("\nThe database is full.\n\n");
+        else
+            printf("\nThe database isn't full yet\n\n");
+    break;
+    }
 }
 
